a7cd14baac0bef6fa6dcdcc4eeeab17a3b94a620
anavisnia <73888794+anavisnia@users.noreply.github.com>
2020-12-03 16:25:14 +0200

    Game time

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const car1 = document.querySelector('.car-1');
+const car2 = document.querySelector('.car-2');
+
+const models = ['car', 'sedanas', 'tesla'];
+const windowWidth = innerWidth;
+const carWidth = 150;
+const finishWidth = 40;
+const finishRightSpace = 10;
+const carStep = 10;
+
+const finishPosition = windowWidth - carWidth - finishWidth - finishRightSpace;
+
+const car1Model = Math.floor(Math.random() * models.length);
+const car2Model = Math.floor(Math.random() * models.length);
+
+car1.dataset.model = models[car1Model];
+car2.dataset.model = models[car2Model];
+
+let car1Position = 0;
+let car2Position = 0;
+
+car1.style.left = car1Position;
+car2.style.left = car1Position;
+
+
+// ovverridina savybes is javascripto css'e
+// car1.style.left = car1Position + 'px';
+// car2.style.left = car2Position + 'px';
+
+
+addEventListener('keyup', (event) => {
+    const key = event.key;
+    if (key === 'w') {
+        car1Position +=carStep;
+        car1Position += 10;
+        car1.style.left = car1Position + 'px';
+        console.log(car1Position);
+        if (car1Position > finishPosition) {
+            alert('Laimejo pirma masina!');
+            location.reload();
+        }
+    }
+    if (key === 'p') {
+        car2Position += carStep;
+        car2Position += 10;
+        car2.style.left = car2Position + 'px';
+        if(car2Position > finishPosition) {
+            alert('Laimejo antra masina!');
+            location.reload();
+        }
+    }
+})
